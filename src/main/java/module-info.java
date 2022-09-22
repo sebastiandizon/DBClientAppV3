994afc7641b4cedb9ba362ef7a99fde0588e1c7a
sebastiandizon <sebastiantingdizon@gmail.com>
2022-09-22 13:32:18 -0700

    +Added libraries +Created JDBC driver +Added helper package +Refactored HelloApplication -> Main (Also controller class) +Tested database connection (Working)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.company.dbclientappv2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.company.dbclientappv2 to javafx.fxml;
+    exports com.company.dbclientappv2;
+}
